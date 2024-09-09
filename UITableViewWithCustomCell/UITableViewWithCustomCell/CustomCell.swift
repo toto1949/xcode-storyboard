e6ecb6ab48e705cf25659be2b3bb988d66727fde
Taoufiq El Moutaouakil <rztaoufiq@gmail.com>
2024-09-09 12:29:42 -0700

    implement MVC pattern

--- UITableViewWithCustomCell/UITableViewWithCustomCell/CustomCell.swift
+++ UITableViewWithCustomCell/UITableViewWithCustomCell/CustomCell.swift
@@ -2,9 +2,9 @@ import UIKit
 
 class CustomCell: UITableViewCell {
     
-    @IBOutlet weak var imageprofile: UIImageView!
+//    @IBOutlet weak var imageprofile: UIImageView!
     @IBOutlet weak var labelTitle: UILabel!
-    @IBOutlet weak var labelDescription: UILabel!
+//    @IBOutlet weak var labelDescription: UILabel!
     //    @IBOutlet weak var labelDescription: UILabel!
     
     override func awakeFromNib() {
--- UITableViewWithCustomCell/UITableViewWithCustomCell/ViewController.swift
+++ UITableViewWithCustomCell/UITableViewWithCustomCell/ViewController.swift
@@ -4,19 +4,19 @@ class ViewController:  UIViewController, UITableViewDataSource {
     
     @IBOutlet weak var tableview: UITableView!
     
-    var launchItems: [LaunchItem] = [
-        LaunchItem(name: "Lunch 1", description: "description 1", launchTime: "2024-09-04 14:30", status: .success),
-        LaunchItem(name: "Lunch 2", description: "description 2", launchTime: "2024-08-15 09:00", status: .failure),
-        LaunchItem(name: "Lunch 3", description: "description 3", launchTime: "2024-07-22 16:33", status: .success),
-    ]
-    
+//    var launchItems: [LaunchItem] = [
+//        LaunchItem(name: "Lunch 1", description: "description 1", launchTime: "2024-09-04 14:30", status: .success),
+//        LaunchItem(name: "Lunch 2", description: "description 2", launchTime: "2024-08-15 09:00", status: .failure),
+//        LaunchItem(name: "Lunch 3", description: "description 3", launchTime: "2024-07-22 16:33", status: .success),
+//    ]
+//    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableview.dataSource = self
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return launchItems.count
+        return 100
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -24,11 +24,13 @@ class ViewController:  UIViewController, UITableViewDataSource {
             fatalError("Unable to dequeue CustomCell")
         }
         
-        let launchItem = launchItems[indexPath.row]
-        cell.labelTitle.text = launchItem.name
-        cell.labelDescription.text = "\(launchItem.description) at \(launchItem.launchTime). Status is \(launchItem.status)"
-
-        cell.imageprofile.image = launchItem.status == .success ? UIImage(systemName: "checkmark.circle") : UIImage(systemName: "xmark.circle")
+//        let launchItem = launchItems[indexPath.row]
+//        cell.labelTitle.text = launchItem.name
+//        cell.labelDescription.text = "\(launchItem.description) at \(launchItem.launchTime). Status is \(launchItem.status)"
+//
+//        cell.imageprofile.image = launchItem.status == .success ? UIImage(systemName: "checkmark.circle") : UIImage(systemName: "xmark.circle")
+        let launchItem = indexPath.row
+                cell.labelTitle.text = String(launchItem)
         
         return cell
     }
